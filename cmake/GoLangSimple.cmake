e422e53a602ee8b8172ee6f38c9ac5a78a06fc8f
Ian Johnson <ian.johnson@canonical.com>
2018-03-23 20:02:10 -0500

    Add new package folder function, ADD_GO_PACKAGE_FOLDER
    This new function is for folders that need some setup/compilation and are meant to be separate from any commands that are part of the library, particularly useful for cgo.

--- cmake/GoLangSimple.cmake
+++ cmake/GoLangSimple.cmake
@@ -112,3 +112,128 @@ function(ADD_GO_INSTALLABLE_PROGRAM)
 			DEPENDS ${GO_PROGRAM_TARGET}Test_copy)
 	endif()
 endfunction(ADD_GO_INSTALLABLE_PROGRAM)
+
+
+# ADD_GO_PACKAGE_FOLDER allows for adding a new go progam target
+function(ADD_GO_PACKAGE_FOLDER)
+	# First parse the arguments
+	set(oneValueArgs TARGET MAIN_FOLDER IMPORT_PATH)
+	set(multiValueArgs TEST_PACKAGES CONFIGURE_FILES BUILD_ENVIRONMENT GET_ENVIRONMENT)
+	cmake_parse_arguments(GO_PACKAGE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+	# This variable tracks the copy of the main file inside the gopath
+	set(GO_PACKAGE_GOPATH ${GOPATH}/src/${GO_PACKAGE_IMPORT_PATH})
+	set(GO_PACKAGE_GOPATH_MAIN_FOLDER ${GO_PACKAGE_GOPATH}/${GO_PACKAGE_MAIN_FOLDER})
+
+	# Add the target for copying the files over
+	add_custom_target(${GO_PACKAGE_TARGET}_copy)
+
+	# First we need to build up the list of files to configure and files to copy over
+	# We can get the files to copy over by globbing the package directory, then iterating over
+	# that list and only adding those files that don't appear inside CONFIGURE_FILES
+	# Also note that this includes all files - not just go files in case there is test data, etc. in the folders
+	# we want to copy that over too
+	file(GLOB_RECURSE GO_PACKAGE_ALL_ITEMS 
+		RELATIVE ${CMAKE_CURRENT_LIST_DIR}
+		${CMAKE_CURRENT_LIST_DIR}/${GO_PACKAGE_MAIN_FOLDER}/* )
+	set(GO_PACKAGE_COPY_FILES "")
+	foreach(PackageItem ${GO_PACKAGE_ALL_ITEMS})
+		# Check this file/directory against all of the specified files that need to be configured
+		set(FILE_FOUND FALSE)
+		foreach(ConfigureFile ${GO_PACKAGE_CONFIGURE_FILES})
+			if(${ConfigureFile} STREQUAL ${PackageItem})
+				# This item should be configured, not copied
+				set(FILE_FOUND TRUE)
+				break()
+			endif()
+		endforeach()
+		if(NOT ${FILE_FOUND})
+			# This package item wasn't in the list of files to be configured, so if it's not a directory we should add it to the list of things to copy
+			# If it's a directory we don't want to copy it, as copying in a directory may inadvertantly copy in any configure files
+			if(NOT IS_DIRECTORY  ${PackageItem})
+				get_filename_component(ABSOLUTE_PACKAGE_FILE ${PackageItem} ABSOLUTE)
+				list(APPEND GO_PACKAGE_COPY_FILES ${ABSOLUTE_PACKAGE_FILE})
+			endif()
+		endif()
+	endforeach(PackageItem)
+
+
+	# Before we can copy all of the files over, we have to make sure that all of the output directories exist
+	foreach(CopyFile ${GO_PACKAGE_COPY_FILES})
+		get_filename_component(CopyFileDirectory ${CopyFile} DIRECTORY)
+		# Add a create this directory
+		add_custom_command(TARGET ${GO_PACKAGE_TARGET}_copy
+				COMMAND ${CMAKE_COMMAND} -E
+				make_directory ${CMAKE_CURRENT_LIST_DIR}/${GO_PACKAGE_MAIN_FOLDER}/${CopyFileDirectory})
+	endforeach(CopyFile)
+
+	# Get the directory of the main folder as the location we are copying files to, this is necessary to ensure
+	# that if nested main folders such as dir1/pkg1 is specified then we copy to the right place
+	get_filename_component(GO_PACKAGE_MAIN_FOLDER_COPY_DEST ${GO_PACKAGE_GOPATH}/${GO_PACKAGE_MAIN_FOLDER} DIRECTORY)
+
+	# Add a custom command to copy the necessary files over
+	add_custom_command(TARGET ${GO_PACKAGE_TARGET}_copy
+			COMMAND ${CMAKE_COMMAND} -E
+			copy ${GO_PACKAGE_COPY_FILES} ${GO_PACKAGE_MAIN_FOLDER_COPY_DEST}
+			# The copy command depends on all of the files that need aren't to be configured
+			DEPENDS ${GO_PACKAGE_COPY_FILES})
+
+	# Configure the files requested to be configured
+	foreach(ConfigureFile ${GO_PACKAGE_CONFIGURE_FILES})
+		message(STATUS "Configuring file ${ConfigureFile}")
+		configure_file("${ConfigureFile}" "${GO_PACKAGE_GOPATH}/${ConfigureFile}")
+	endforeach(ConfigureFile)
+
+	# Add the actual build target which depends on the files being updated
+	add_custom_target(${GO_PACKAGE_TARGET} ALL)
+	add_dependencies(${GO_PACKAGE_TARGET} ${GO_PACKAGE_TARGET}_copy)
+
+	# First before building the target, we automatically fetch all of the dependencies declared 
+	# in the go file using go get
+	add_custom_command(TARGET ${GO_PACKAGE_TARGET}
+		COMMAND ${CMAKE_COMMAND} -E env ${GO_PACKAGE_GET_ENVIRONMENT} GOPATH=${GOPATH} go get -v ${CMAKE_GO_FLAGS} -d -t ./...
+		WORKING_DIRECTORY ${GO_PACKAGE_GOPATH_MAIN_FOLDER}
+		DEPENDS ${GO_PACKAGE_TARGET}_copy)
+
+	# Now actually setup the build to go build all of the packages inside of the package folder
+	add_custom_command(TARGET ${GO_PACKAGE_TARGET}
+		COMMAND ${CMAKE_COMMAND} -E env ${GO_PACKAGE_BUILD_ENVIRONMENT} GOPATH=${GOPATH} go build -v ${CMAKE_GO_FLAGS}  ./...
+		WORKING_DIRECTORY ${GO_PACKAGE_GOPATH_MAIN_FOLDER}
+		DEPENDS ${GO_PACKAGE_TARGET}_copy)
+
+	# TODO: see if there's anything we should do about installing this package folder
+
+	# TODO: finish implementing automatic tests for packages
+	# Now check if we should add tests for this package
+	# file(GLOB GO_PACKAGE_TEST_FILES *_test.go)
+	# if(${GO_PACKAGE_TEST_FILES})
+	# 	# This is to support multiple packages being specified for a single cover profile with go test
+	# 	# but requires golang 1.10, see https://github.com/golang/go/issues/6909
+	# 	# Then the call to go test can use the variable GO_PACKAGE_FULL_TEST_PACKAGES and get more accurate testing results
+	# 	set(GO_PACKAGE_FULL_TEST_PACKAGES "")
+	# 	foreach(test_pkg ${GO_PACKAGE_TEST_PACKAGES})
+	# 		list(APPEND GO_PACKAGE_FULL_TEST_PACKAGES ${GO_PACKAGE_IMPORT_PATH}/${test_pkg})
+	# 	endforeach(test_pkg)
+	# 	get_filename_component(GO_PROGRAM_TEST_FILE_NAME ${GO_PROGRAM_MAIN_SOURCE_TEST_FILE} NAME)
+	# 	message(STATUS "Found go test file : ${MAIN_SRC_DIR}/${GO_PROGRAM_TEST_FILE_NAME}")
+	# 	message(STATUS "Enabling testing for ${GO_PACKAGE_TARGET}")
+	# 	# Add a dummy test command to copy the test file over 
+	# 	add_test(NAME ${GO_PACKAGE_TARGET}Test_copy
+	# 		COMMAND "${CMAKE_COMMAND}" -E
+	# 		copy ${MAIN_SRC_ABS_DIR}/${GO_PROGRAM_TEST_FILE_NAME} ${GO_PACKAGE_GOPATH}/${MAIN_SRC_DIR}/${GO_PROGRAM_TEST_FILE_NAME})
+
+	# 	# Add the go test command
+	# 	add_test(NAME ${GO_PACKAGE_TARGET}Test
+	# 		COMMAND ${CMAKE_COMMAND} -E env GOPATH=${GOPATH} go test ${GO_PACKAGE_FULL_TEST_PACKAGES} -coverprofile=${GO_COVERAGE_DIRECTORY}/${GO_PACKAGE_TARGET}coverage.out
+	# 		WORKING_DIRECTORY ${GO_PROGRAM_GOPATH_MAIN_SOURCE_DIR})
+
+	# 	# Also add a coverage html generation output - this will generate coverage statistics in a HTML viewer of the code
+	# 	add_test(NAME ${GO_PACKAGE_TARGET}coverage
+	# 		COMMAND ${CMAKE_COMMAND} -E env GOPATH=${GOPATH} go tool cover -o ${GO_COVERAGE_DIRECTORY}/${GO_PACKAGE_TARGET}.html -html=${GO_COVERAGE_DIRECTORY}/${GO_PACKAGE_TARGET}coverage.out
+	# 		WORKING_DIRECTORY ${GO_PROGRAM_GOPATH_MAIN_SOURCE_DIR})
+
+	# 	# Make the test target dependent on the copy target
+	# 	set_tests_properties(${GO_PACKAGE_TARGET}Test PROPERTIES 
+	# 		DEPENDS ${GO_PACKAGE_TARGET}Test_copy)
+	# endif()
+endfunction(ADD_GO_PACKAGE_FOLDER)
